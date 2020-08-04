4ab8115f49ac2802243aaaae7de07fad066ebda4
Emma <imgenu@gmail.com>
2020-08-04 16:51:52 +0800

    Updates

---
+++ precache-manifest.9036c4ff80aa198d3db4a5f0bdadcd63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f22a590e4d662f5776f3676943282399",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "582c973ec3e84b147e76",
+    "url": "/todolist/static/css/main.a72a4fd2.chunk.css"
+  },
+  {
+    "revision": "8b70247e6098a3e3dbe5",
+    "url": "/todolist/static/js/2.e300a289.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist/static/js/2.e300a289.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "582c973ec3e84b147e76",
+    "url": "/todolist/static/js/main.67a815ba.chunk.js"
+  },
+  {
+    "revision": "0bb9fb55b77e7091c3db",
+    "url": "/todolist/static/js/runtime-main.31a253ee.js"
+  }
+]);
